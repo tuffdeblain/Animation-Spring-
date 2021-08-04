7836f54c9896a6d212fb95545c43cc6723470a66
tuffdeblain <himanfullhd@gmail.com>
2021-08-05 00:17:14 +0500

    Initial Commit

---
+++ KudinovSA_HW2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KudinovSA_HW2.9
+//
+//  Created by Сергей Кудинов on 05.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
